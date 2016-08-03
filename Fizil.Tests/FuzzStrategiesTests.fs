bdff9c9207b8517d33d3d0e444db2e0a7006a6e0
Craig Stuntz <craigstuntz@gmail.com>
2016-08-02 23:41:45 -0400

    Add arith8 fuzz strategy

--- Fizil.Tests/FuzzStrategiesTests.fs
+++ Fizil.Tests/FuzzStrategiesTests.fs
@@ -49,3 +49,49 @@ module FuzzStrategiesTest =
         let fuzzed = FuzzStrategies.byteFlip 1 input
         let actual = List.ofSeq fuzzed.TestCases
         Assert.That(actual, Is.EqualTo expected)
+
+    [<Test>]
+    let ``couldBeBitflip 0 1 is true``() = 
+        let actual = FuzzStrategies.couldBeBitflip(0u, 1u)
+        Assert.That(actual, Is.True)
+
+    [<Test>]
+    let ``couldBeBitflip 0 0 is true``() = 
+        let actual = FuzzStrategies.couldBeBitflip(0u, 0u)
+        Assert.That(actual, Is.False)
+
+    [<Test>]
+    let ``couldBeBitflip with non-bitflipped values is false``() = 
+        let oldValue = 2863311530u // 10101010101010101010101010101010
+        let actual = FuzzStrategies.couldBeBitflip(oldValue, 0u)
+        Assert.That(actual, Is.False)
+
+    [<Test>]
+    let ``arith8 returns expected values``() = 
+        let input = [| 128uy |]
+        let fuzzed = FuzzStrategies.arith8 input
+        let expected = 
+            seq [ 
+                [| 113uy |] 
+                [| 114uy |] 
+                [| 115uy |] 
+                [| 116uy |] 
+                [| 117uy |] 
+                [| 118uy |] 
+                [| 119uy |] 
+                [| 120uy |] 
+                [| 121uy |] 
+                [| 122uy |] 
+                [| 123uy |] 
+                [| 124uy |] 
+                [| 125uy |] 
+                [| 126uy |] 
+                [| 133uy |] 
+                [| 135uy |] 
+                [| 137uy |] 
+                [| 138uy |] 
+                [| 139uy |] 
+                [| 141uy |] 
+                [| 142uy |] 
+            ]
+        Assert.That(fuzzed.TestCases, Is.EqualTo expected)
--- Fizil/FuzzStrategies.fs
+++ Fizil/FuzzStrategies.fs
@@ -89,17 +89,68 @@ let byteFlip (flipBytes: int) : FuzzStrategy =
             TestCases = testCases
         }
 
+/// Helper function to see if a particular change could
+/// be a product of deterministic bit flips with the lengths and stepovers
+/// attempted by the fuzzer. This is used to avoid dupes in some of the
+/// deterministic fuzzing operations that follow bit flips. We also
+/// return true if xoredOldAndNew is zero, which implies that the old and 
+/// attempted new values are identical and the exec would be a waste of time.
+let couldBeBitflip(oldValue: uint32, newValue: uint32) : bool =
+    // implementation is more or less a direct translation of similar
+    // function in afl-fuzz
+    let xoredOldAndNew = oldValue ^^^ newValue
+    let rec loop (value: uint32, shiftAmount: uint32) =
+        match value with 
+        | 0u -> false
+        | _ when (value &&& 1u = 0u) ->
+            let shifted = value >>> 1
+            let newShiftAmount = shiftAmount + 1u
+            loop(shifted, newShiftAmount)
+        | 1u | 3u | 15u -> true
+        | _ when (shiftAmount &&& 7u = 7u) ->
+            // 8-, 16-, and 32-bit patterns are OK only if shift factor is
+            // divisible by 8, since that's the stepover for these ops.
+            false
+        | 0xffu | 0xffffu | 0xffffffffu -> true
+        | _ -> false
+    loop (xoredOldAndNew, 0u)
+
+
+let arithMax = 16uy;
+
+let arith8 : FuzzStrategy =
+    fun (bytes: byte[]) ->
+        let testCases = seq {
+            for i = 0 to bytes.Length - 1 do
+                let oldByte = bytes.[i]
+                let newBytes = 
+                    Array.concat [|
+                        ([| 1uy .. arithMax |] |> Array.rev |> Array.map (fun offset -> oldByte - offset))
+                        ([| 1uy .. arithMax |] |> Array.map (fun offset -> oldByte + offset)) |]
+                for newByte in newBytes do
+                    if not (couldBeBitflip(uint32(oldByte), uint32(newByte)))
+                    then 
+                        let newBytes = Array.copy bytes
+                        newBytes.[i] <- newByte
+                        yield newBytes
+        }
+        {
+            Name = "arith 8/8"
+            TestCases = testCases
+        }
 
 /// An ordered list of functions to use when starting with a single piece of 
 /// example data and producing new examples to try
 let all = [ 
     useOriginalExample
+    arith8
     bitFlip 1
     bitFlip 2
     bitFlip 4
     byteFlip 1
     byteFlip 2
     byteFlip 4
+    
 ]
                 
 

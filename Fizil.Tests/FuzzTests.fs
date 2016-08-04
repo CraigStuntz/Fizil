cf736e5a4172bc2132d2597df48efe4d5242da59
Craig Stuntz <craigstuntz@gmail.com>
2016-08-03 22:32:20 -0400

    Consolidate Fuzz module

--- Fizil.Tests/FuzzTests.fs
+++ Fizil.Tests/FuzzTests.fs
@@ -2,12 +2,12 @@
 
 open NUnit.Framework
 
-module FuzzStrategiesTest = 
+module FuzzTest = 
 
     [<Test>]
     let ``useOriginalExample returns originals``() = 
         let input = [| 0uy |]
-        let fuzzed = FuzzStrategies.useOriginalExample input
+        let fuzzed = Fuzz.useOriginalExample input
         Assert.That(fuzzed.TestCases |> Seq.length, Is.EqualTo 1)
         Assert.That(fuzzed.TestCases |> Seq.head |> Seq.head, Is.EqualTo (input |> Array.head))
 
@@ -33,7 +33,7 @@ module FuzzStrategiesTest =
             [| 0b00000000uy; 0b10111111uy |]
             [| 0b00000000uy; 0b01111111uy |] 
         ]
-        let fuzzed = FuzzStrategies.bitFlip 1 input
+        let fuzzed = Fuzz.bitFlip 1 input
         let actual = List.ofSeq fuzzed.TestCases
         Assert.That(actual, Is.EqualTo expected)
 
@@ -46,30 +46,30 @@ module FuzzStrategiesTest =
             [| 0b00000000uy; 0b00000000uy; 0b00000000uy |]
             [| 0b00000000uy; 0b11111111uy; 0b11111111uy |]
         ]
-        let fuzzed = FuzzStrategies.byteFlip 1 input
+        let fuzzed = Fuzz.byteFlip 1 input
         let actual = List.ofSeq fuzzed.TestCases
         Assert.That(actual, Is.EqualTo expected)
 
     [<Test>]
     let ``couldBeBitflip 0 1 is true``() = 
-        let actual = FuzzStrategies.couldBeBitflip(0u, 1u)
+        let actual = Fuzz.couldBeBitflip(0u, 1u)
         Assert.That(actual, Is.True)
 
     [<Test>]
     let ``couldBeBitflip 0 0 is true``() = 
-        let actual = FuzzStrategies.couldBeBitflip(0u, 0u)
+        let actual = Fuzz.couldBeBitflip(0u, 0u)
         Assert.That(actual, Is.False)
 
     [<Test>]
     let ``couldBeBitflip with non-bitflipped values is false``() = 
         let oldValue = 2863311530u // 10101010101010101010101010101010
-        let actual = FuzzStrategies.couldBeBitflip(oldValue, 0u)
+        let actual = Fuzz.couldBeBitflip(oldValue, 0u)
         Assert.That(actual, Is.False)
 
     [<Test>]
     let ``arith8 returns expected values``() = 
         let input = [| 128uy |]
-        let fuzzed = FuzzStrategies.arith8 input
+        let fuzzed = Fuzz.arith8 input
         let expected = 
             seq [ 
                 [| 113uy |] 
